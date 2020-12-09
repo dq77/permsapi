9d1a2ab6929f6b338f7b2c0d471283aa3ba20367
掉漆 <270778319@qq.com>
2020-12-09 17:27:27 +0800

    优化保存

--- models/user.js
+++ models/user.js
@@ -4,11 +4,28 @@
  * @LastEditors: わからないよう
  */
 var db = require('../dataBase/mysql');
+const moment = require('moment')
 var User = {};
 
 // 获取用户信息
 User.find = function(artid, callback) {
   var sql = "SELECT * FROM `perms`.`permtable` WHERE `artid` = '"+artid+"' LIMIT 0, 10;";
+  db.query(sql, function(err, rows) {
+    callback(err, rows)
+  });
+};
+
+// 查询历史记录
+User.getOld = function(artid, callback) {
+  var sql = "SELECT * FROM `perms`.`history` WHERE `artid` = '"+artid+"' ORDER BY `savetime` DESC LIMIT 0, 10;";
+  db.query(sql, function(err, rows) {
+    callback(err, rows)
+  });
+};
+
+// 保存历史记录
+User.saveOld = function(artid, val, callback) {
+  var sql = "INSERT INTO `perms`.`history` (artid, val, savetime) VALUES ("+artid+", '"+val+"', '"+moment(new Date()).format('YYYY-MM-DD HH:mm:ss')+"');";
   db.query(sql, function(err, rows) {
     callback(err, rows)
   });
--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,34 @@ router.post('/getartinfo', function(req, res, next) {
 
 /* POST 带参数请求 */
 router.post('/saveart', function(req, res, next) {
-  User.setArtInfo(req.body.artid, req.body.val, function(err, result){
+  User.find(req.body.artid, function(err, result){
+    let resp = {}
+    if(err){
+      User.addArtInfo(req.body.artid, req.body.val, function(err, result1){
+        if(err){
+          res.send('not found');
+        }
+        resp = result1
+      });
+    } else {
+      User.setArtInfo(req.body.artid, req.body.val, function(err, result2){
+        if(err){
+          res.send('not found');
+        }
+        resp = result2
+      });
+    }
+    if (result[0] && result[0].id) {
+      User.saveOld(req.body.artid, result[0].val, function(err, result){
+      });
+    }
+    res.send(resp);
+  });
+});
+
+/* 获取历史记录 */
+router.post('/gethistory', function(req, res, next) {
+  User.getOld(req.body.artid, function(err, result){
     if(err){
       res.send('not found');
     }
