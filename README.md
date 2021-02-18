035942c2dac942579b32e7fb8be72041e4b2789f
掉漆 <270778319@qq.com>
2021-02-18 14:12:39 +0800

    init readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 <!--
  * @Author: 刁琪
  * @Date: 2020-11-30 16:10:48
- * @LastEditors: わからないよう
+ * @LastEditors: 掉漆
 -->
 # permsapi
 个人笔记node端代码
-etc/nodefront/permsapi
+存放位置：etc/nodefront/permsapi
+Linux先安装MySql并配置好远程连接
+forever运行app.js即可
+反向代理配置：
+```
+location /picert {
+    rewrite ^/picert/(.*)$ /$1 break;
+    proxy_pass http://123.123.123.123:9305;
+}
+```
--- dataBase/dbConfig.js
+++ dataBase/dbConfig.js
@@ -1,18 +1,18 @@
 /*
  * @Author: 刁琪
  * @Date: 2020-11-24 14:28:56
- * @LastEditors: わからないよう
+ * @LastEditors: 掉漆
  */
 let mysql = {
 
-  host:"154.222.1.52", // 测试环境
+  host:"111.111.111.111", // 测试环境
   user:"root", // 测试环境
 
   // host:"rm-bp1z8j25j2i627qewso.mysql.rds.aliyuncs.com", // 生产环境
   // user:"readonly", // 生产环境
 
   port: "3306",
-  password:"ddqq19956-",
+  password:"Ddqq19956-",
   database:"perms"
 }
 module.exports = mysql;
